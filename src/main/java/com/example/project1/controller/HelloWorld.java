43a3b97fc24a3258a05200e1368c0e97039fd811
Surya02 <ssurya2910@gmail.com>
2024-04-27 13:02:41 +0530

    added post,patch api example

--- src/main/java/com/example/project1/controller/HelloWorld.java
+++ src/main/java/com/example/project1/controller/HelloWorld.java
@@ -1,7 +1,6 @@
 package com.example.project1.controller;
 
 import com.example.project1.dto.HelloWorldDto;
-import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -9,7 +8,7 @@ import java.util.Arrays;
 @RestController
 @RequestMapping("/api")
 public class HelloWorld {
-    int[] arr = {4};
+    int[] arr = {4,5,6};
 
     @GetMapping("/")
     public int[] getAllNumbers(){
@@ -38,10 +37,37 @@ public class HelloWorld {
         arr= Arrays.copyOf(arr,arr.length-1);
         return arr;
     }
-
     @PostMapping("/")
-    public int[] createNumbers(@RequestBody HelloWorldDto helloWorldDto){
+    public int[] insertNumbers(@RequestBody HelloWorldDto helloWorldDto ){
+        int [] insertArray = helloWorldDto.getNumbers();
+
+        int aLen = arr.length;
+        int bLen = insertArray.length;
+        int[] result = new int[aLen + bLen];
+
+        System.arraycopy(arr, 0, result, 0, aLen);
+        System.arraycopy(insertArray, 0, result, aLen, bLen);
+
+        arr=result;
+
+        return result;
+    }
+    @PutMapping("/")
+    public int[] updateNumbers(@RequestBody HelloWorldDto helloWorldDto){
         arr = helloWorldDto.getNumbers();
+        return arr;
+    }
+    @PatchMapping("/{index}")
+    public int[] editNumber(@PathVariable(name="index",required=true)String index,@RequestBody HelloWorldDto helloWorldDto){
+        int indexNumber=Integer.parseInt(index);
+        int [] insertArray = helloWorldDto.getNumbers();
+        int aLen = arr.length;
+        int bLen = insertArray.length;
+        int[] result = new int[(aLen+bLen)-1];
+        System.arraycopy(arr, 0, result, 0, indexNumber);
+        System.arraycopy(insertArray, 0, result, indexNumber, bLen);
+        System.arraycopy(arr, indexNumber+1, result, indexNumber + bLen, aLen-(indexNumber+1));
+        arr=result;
         return arr;
     }
 }
--- src/main/java/com/example/project1/dto/HelloWorldDto.java
+++ src/main/java/com/example/project1/dto/HelloWorldDto.java
@@ -10,5 +10,4 @@ import lombok.*;
 public class HelloWorldDto {
     @JsonProperty("numbers")
     private int[] numbers;
-
 }
